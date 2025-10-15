773df3e811b8d0183abfc5c3b92d0699eaf28501
François Roland <francois.roland@heh.be>
2025-10-15 09:00:20 +0200

    Update instructions and tests

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-# Module - TP1 - Titre
+# Chaines de caractères - TP1 - Comptage d’occurrences d’une lettre dans un mot
 
 ## Consignes
+
+Écris une fonction `compterOccurrencesLettre`qui détermine le nombre de fois qu’une lettre donnée apparaît dans un mot.
+Le mot et la lettre à rechercher sont tous deux définis directement dans le code.
 
 **Signature de la fonction :**
 
 ```ts
-function fct(param: boolean): boolean;
+function compterOccurrencesLettre(mot: string, lettre: string): string
 ```
 
 ## Exemples
+
+`compterOccurrencesLettre ("Programmation","m")` doit retourner `La lettre 'm' apparaît 2 fois`
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,11 @@
-// Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
+import { compterOccurrencesLettre } from "../index";
 
-import {...} from '../index';
+describe("Comptage d'occurrences d'une lettre dans un mot", () => {
+  test('Premier test avec le mot "Programmation" et la lettre "m"', () => {
+    expect(compterOccurrencesLettre("Programmation", "m")).toEqual("La lettre 'm' apparaît 2 fois");
+  });
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
-    });
+  test('Deuxième test avec le mot "HeH - Haute Ecole en Hainaut" et la lettre "e"', () => {
+    expect(compterOccurrencesLettre("HeH - Haute Ecole en Hainaut", "e")).toEqual("La lettre 'e' apparaît 5 fois");
+  });
 });
